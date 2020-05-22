3a79ebb8669bfbd4959200ed40ee6fe4b84874e0
kwakyongkee <kyk0185@gmail.com>
2020-05-22 21:43:10 +0900

    htmlLayout 리펙토링

--- src/HtmlLayout.js
+++ src/HtmlLayout.js
@@ -1,39 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { userInfo,data } from './dummy';
+import { userInfo, data } from './dummy';
 import './HtmlLayout.css';
 
 class HtmlLayout extends Component {
     constructor(props) {
-        console.log(props)
         super(props)
         this.state = {
-            filename: "",
-            filenameId:"",
+            dummyData : data,
+
+            //삭제예정
+            fileName: "",
+            filenameId: "",
             deleteId: "",
             role_name: "",
-            code:""
+            code: ""
         }
-
     }
 
     componentDidMount = () => {
         for (let i = 0; i < userInfo.roles.length; i++) {
             if (userInfo.roles[i].role_no == this.props.match.params['role_no']) {
-                this.setState({ role_name: userInfo.roles[i].role_name,code:userInfo.roles[i].code })
+                this.setState({ role_name: userInfo.roles[i].role_name, code: userInfo.roles[i].code })
             }
         }
 
         //window.addEventListener("focus", () => {
-            for (let i = 0; i < this.props.data.addItem.length; i++) {
-                 if (this.props.data.addItem[i].filename !== '') {
-                     return (
-                         this.setState({ filename: this.props.data.addItem[i].filename, filenameId:this.props.data.addItem[i].filenameId})
-                     )
-                 }
-             }
+        for (let i = 0; i < this.props.data.addItem.length; i++) {
+            if (this.props.data.addItem[i].fileName !== '') {
+                return (
+                    this.setState({ fileName: this.props.data.addItem[i].fileName, filenameId: this.props.data.addItem[i].filenameId })
+                )
+            }
+        }
         //})
+
+        this.onRefresh();
+    }
+
+    onRefresh =_=> {
+        const data = JSON.parse(window.localStorage.getItem("dummyData"));
+
+        if (data !== null && data.length > 0) {
+            this.setState({ dummyData: data })
+        } 
     }
 
     logOut = () => {
@@ -41,24 +52,29 @@ class HtmlLayout extends Component {
     }
     fileUpload = e => {
         let files = e.target.files;
-        //this.setState({ filename: files[0].name });
+        //this.setState({ fileName: files[0].name });
     }
     // 파일업로드 활성화/비활성화
     //renderSection = () => {
-      //  for (let i = 0; i < this.props.data.addItem.length; i++) {
-        //    if (this.props.data.addItem[i].deleteYn === true) {
-          //      return (
-            //        <h3>{this.props.data.addItem[i].filename}</h3>
-              //  )
-            //} else {
-              //  return (
-                //    <h3></h3>
-               // )
-           // }
-       // }
+    //  for (let i = 0; i < this.props.data.addItem.length; i++) {
+    //    if (this.props.data.addItem[i].deleteYn === true) {
+    //      return (
+    //        <h3>{this.props.data.addItem[i].fileName}</h3>
+    //  )
+    //} else {
+    //  return (
+    //    <h3></h3>
+    // )
+    // }
+    // }
     //}
 
     render() {
+
+        const { dummyData } = this.state;
+
+        console.log(dummyData);
+
         return (
             <div className="frame">
                 <div className="header" >
@@ -67,6 +83,9 @@ class HtmlLayout extends Component {
                         <Link to="/admin" className="cover"> 등록</Link>
                         <button className="cover" onClick={this.logOut}>
                             로그아웃
+                        </button>
+                        <button className="cover" onClick={this.onRefresh}>
+                            새로고침
                         </button>
 
                     </div>
@@ -168,23 +187,23 @@ class HtmlLayout extends Component {
                             <div className="section2_2 blueviolets" />
                         </div>
                     </div>
-                    {data.map((item, index) => (
+                    {dummyData.map((item, index) => (
                         <div className="content3">
-                            {item.title.map((items, index) => (
-                                <div>
-                                    {items.code === this.state.code &&
-                                        <div className="section3 grays" key={items.key}>
-                                            {items.key === this.state.filenameId &&
+                            {
+                                item.titles.map((title, titleIndex) => (
+                                    <div>
+                                        <div className="section3 grays" key={`title-${index}-${titleIndex}`}>
+                                            {
                                                 <div className="columnAlign filebox">
-                                                    <h3>{this.state.filename}</h3>
+                                                    <h3>{title.fileName}</h3>
                                                     <label htmlFor="ex_file">파일 업로드</label>
                                                     <input type="file" id="ex_file" onChange={e => this.fileUpload(e)} />
                                                 </div>
                                             }
-                                        </div> 
-                                    }
-                                </div>
-                            ))}
+                                        </div>
+                                    </div>
+                                ))
+                            }
                         </div>
                     ))}
                     <div className="content4">
