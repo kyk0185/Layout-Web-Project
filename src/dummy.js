55affee42a2f3b6b489c5a3a421557b6c57f3ee0
kwakyongkee <kyk0185@gmail.com>
2020-05-22 20:01:52 +0900

    * dummy data 리펙토링

--- src/dummy.js
+++ src/dummy.js
@@ -1,101 +1,85 @@
 export const data = [
     {
-        key: 1, //고유번호
-        sido:'서울시교육청',    //시도명
-        titles:  //소제목 목록
+        key: 1,
+        sido:'서울시교육청',
+        code:'B10',
+        titles:
         [
             {
                 key:1,
                 name:'제목1',
-                code:'B10',
-                fileName: 'test1.exe',
             },
             {
                 key:2,
                 name:'제목2',
-                code:'B10',
-                fileName: 'test2234.exe',
             },
             {
                 key:3,
                 name:'제목3',
-                code:'B10',
-                fileName: 'test3.exe',
             },
             {
                 key:4,
                 name:'제목4',
-                code:'B10',
-                fileName: 'test4.exe',
             }
         ]
     },
     {
         key: 2,
         sido:'인천시교육청',
+        code:'E10',
         titles:
         [
             {
-                key:11,
+                key:1,
                 name:'제목1',
-                code:'E10'
             },
             {
-                key:21,
+                key:2,
                 name:'제목2',
-                code:'E10'
             },
             {
-                key:31,
+                key:3,
                 name:'제목3',
-                code:'E10'
             },
             {
-                key:41,
+                key:4,
                 name:'제목4',
-                code:'E10'
             },
             {
-                key:51,
+                key:5,
                 name:'제목5',
-                code:'E10'
             },
             {
-                key:61,
+                key:6,
                 name:'제목6',
-                code:'E10'
             }
         ]
     },
     {
         key: 3,
         sido:'부산시교육청',
+        code:'C10',
         titles:
         [
             {
-                key:12,
+                key:1,
                 name:'제목1',
-                code:'C10'
             },
             {
-                key:22,
+                key:2,
                 name:'제목2',
-                code:'C10'
             },
             {
-                key:32,
+                key:3,
                 name:'제목3',
-                code:'C10'
             },
             {
-                key:42,
+                key:4,
                 name:'제목4',
-                code:'C10'
             },
             {
-                key:52,
+                key:5,
                 name:'제목5',
-                code:'C10'
             }
         ]
     },
